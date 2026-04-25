b1ff03ebaabf9c70750912e519cb61b41f39065c
syss69 <art435sys@gmail.com>
2026-04-25 17:23:05 +0200

    Логика пополнения изменена. Платеж теперь можно либо подтвердить либо отменить

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -14,6 +14,7 @@ import {
   handleShop,
   handleTopUp,
   handleCheckTopUp,
+  handleCancelTopUp,
   handleShopUnavailable,
   handleTopUpAmount,
 } from "./handlers/shop";
@@ -59,6 +60,7 @@ export function createBot(token: string): Bot<BotContext> {
   bot.callbackQuery("top_up", handleTopUp);
   bot.callbackQuery("enter_promo", handleEnterPromo);
   bot.callbackQuery(/^check_topup:/, handleCheckTopUp);
+  bot.callbackQuery(/^cancel_topup:/, handleCancelTopUp);
   bot.callbackQuery("download_app", handleDownloadApp);
   bot.callbackQuery("back_to_menu", handleBackToMenu);
 
